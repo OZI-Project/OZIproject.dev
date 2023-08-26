a28c0e1b4ef108dfe56c508663dea023ba4486ce
rjdbcm <rjdbcm@mail.umkc.edu>
2023-08-26 16:48:29 -0500

    add compatiblity for multi browser accessibility.

--- assets/js/accessibility.js
+++ assets/js/accessibility.js
@@ -1,6 +1,14 @@
+
 document.getElementsByClassName("hamb")[0].addEventListener("keyup", function(event) {
+    if (event.isComposing || event.keyCode === 229) {
+        return;
+        }
     event.preventDefault();
     if (event.keyCode === 13) {
-        document.getElementById("side-menu").checked = !document.getElementById("side-menu").checked;
+        const checked = document.getElementById("side-menu").checked
+        document.getElementById("hamb-menu-btn").click();
+        if (!checked != document.getElementById("side-menu").checked) {
+            document.getElementById("side-menu").checked = !checked;
         }
+    }
 });
